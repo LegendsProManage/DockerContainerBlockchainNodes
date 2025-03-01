646aeaca80216f2e9855e9ebb5d42298c28d5e5c
LegendsProManage <Legendsprojectmanagers@gmail.com>
2025-03-01 11:14:03 +0530

    Create animations.js

---
+++ dashboard/animations.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = 'translateY(-10px)';
+        card.style.boxShadow = '0 20px 30px rgba(255, 255, 0, 0.4)';
+    });
+    
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'translateY(0)';
+        card.style.boxShadow = 'none';
+    });
+});
